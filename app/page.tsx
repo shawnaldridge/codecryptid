487ef90b42b99b3f020132b9e71a351042380ea1
Shawn Aldridge <raskolnikov42@gmail.com>
2025-05-07 22:18:36 -0700

    tabula rasa

--- app/page.tsx
+++ app/page.tsx
@@ -137,8 +137,7 @@ export default function Personal() {
       >
         <div className="flex-1">
           <p className="text-zinc-600 dark:text-zinc-400">
-            Focused on creating intuitive and performant web experiences.
-            Bridging the gap between design and development.
+            Your work means nothing if nobody can understand it.
           </p>
         </div>
       </motion.section>
